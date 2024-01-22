d04bd23acab6d8cd46491f8041eeb64ec0300548
mvrlxc <mvrlxc@yandex.ru>
2024-01-22 14:30:52 +0300

    daun

--- app/src/main/java/com/example/sirusbob/SirusBobApp.kt
+++ app/src/main/java/com/example/sirusbob/SirusBobApp.kt
@@ -8,21 +8,3 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.sirusbob.R
 
-enum class Screens(@StringRes val title: Int) {
-    MAINSCREEN(title = R.string.app_namee)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SirusBobAppBar(
-    currentScreen: Screens
-) {
-TopAppBar(title = { currentScreen.title} )
-}
-
-@Composable
-fun SirusBobApp() {
-    val navController: NavHostController = rememberNavController()
-
-
-}
